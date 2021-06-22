81b90790c0105c95b9a3e33c69d00fa26cdf43e6
almondhouse <almondhouse@axlrdata.com>
2021-06-22 09:02:04 +0530

    notification added

--- src/admin/DashboardHeader.js
+++ src/admin/DashboardHeader.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Layout, Menu, Dropdown , Breadcrumb, Table, Tag, Space, Avatar, Divider, Tooltip } from "antd";
-import { UserOutlined, DownOutlined  } from '@ant-design/icons';
+import { Layout, Menu, Dropdown , Breadcrumb, Table, Tag, Space, Avatar, Divider, Tooltip, Popover } from "antd";
+import { UserOutlined, DownOutlined, BellOutlined, MailOutlined  } from '@ant-design/icons';
 
 import Logo from "../images/logo.svg";
 const { Header, Content, Footer } = Layout;
@@ -13,6 +13,25 @@ function Index() {
           <Menu.Item key="2">Logout</Menu.Item>
         </Menu>
       );
+      const content = (
+        <div className="notification_content">
+          <div className="item">
+            <MailOutlined />
+            <div className="details">
+            <h6>Content sfsfs sfsfs sf dsf sdf sdfsd fdsf dsfdsf dsfdsfds</h6>
+            <p>Content</p>
+            </div>
+          </div>
+          <div className="item">
+            <MailOutlined />
+            <div className="details">
+            <h6>Content sfsfs sfsfs sf dsf sdf sdfsd fdsf dsfdsf dsfdsfds</h6>
+            <p>Content</p>
+            </div>
+          </div>
+        </div>
+      );
+      
   return (
     <>
       <section className="dashboard_section">
@@ -25,19 +44,26 @@ function Index() {
                     Active Physio Gym
                   </Link>
                 </div>
-                <div className="col-sm-4 text-end">
+                <div className="col-sm-4 text-end d-flex align-items-center justify-content-end">
                 <Tooltip title="Veera" placement="top">
-        <Avatar style={{ backgroundColor: '#1890ff', marginRight:10 }} icon={<UserOutlined />} />
+        <Avatar style={{ backgroundColor: '#1890ff' }} icon={<UserOutlined />} />
       </Tooltip>
+                
                 <Dropdown
         overlay={menu}
         // onVisibleChange={this.handleVisibleChange}
         // visible={this.state.visible}
       >
-        <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+        <a className="ant-dropdown-link" onClick={e => e.preventDefault()} style={{marginLeft:10, color:"#fff"}}>
         Veera <DownOutlined />
         </a>
       </Dropdown>
+      <Popover content={content} trigger="click">
+       <div className="notification-info">
+       <Avatar icon={<BellOutlined />} />
+        <span className="count">2</span>
+       </div>
+      </Popover>
                 </div>
               </div>
             </div>
